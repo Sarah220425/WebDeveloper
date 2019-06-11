ddcf077acd4c7a1f15c096f62d7fb35d2bcd10d2
Sarah220425 <41308291+Sarah220425@users.noreply.github.com>
2019-06-11 22:31:32 +0200

    Add files via upload

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,12 @@
+(function () {
+var helloSpeaker = {};
+var speakWord = "Hello";
+helloSpeaker.speak = function speak(name){
+	console.log(speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+
+})();
+
+
